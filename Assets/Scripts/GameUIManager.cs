16589d6e78115271b9a00df127a6a6b0d2f15350
preranaprakashh <preranaprakash75@gmail.com>
2022-02-04 13:16:13 +0530

    Game contents added

---
+++ Assets/Scripts/GameUIManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUIManager : MonoBehaviour
+{
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void HomeButton()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+}
